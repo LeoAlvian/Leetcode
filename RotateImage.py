b6113e29b07dcded2dedf53f843751bb4ae3c5bc
Leo <alvianleo@gmail.com>
2025-09-13 15:56:33 +0700

    adding RotateImage.py

---
+++ RotateImage.py
@@ -0,0 +1,35 @@
+"""
+You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees
+(clockwise).
+
+You have to rotate the image in-place, which means you have to modify the input 2D 
+matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+"""
+
+matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+out = [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+
+
+def rotate(matrix):
+    left, right = 0, len(matrix) - 1
+
+    while left < right:
+        top, bottom = left, right
+        for i in range(right - left):
+            topLeft = matrix[top][left + i]
+            matrix[top][left + i] = matrix[bottom - i][left]
+            matrix[bottom - i][left] = matrix[bottom][right - i]
+            matrix[bottom][right - i] = matrix[top + i][right]
+            matrix[top + i][right] = topLeft
+        left += 1
+        right -= 1
+
+
+
+print(matrix)
+rotate(matrix)
+print(matrix)
+print(out == matrix)
