3338c3a58fe1db22aa6ea43ae9093b743928f65a
Leo <alvianleo@gmail.com>
2025-09-13 16:00:11 +0700

    Modify and adding documentation/comment to the RotateImage.py

--- RotateImage.py
+++ RotateImage.py
@@ -14,19 +14,32 @@ out = [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
 
 
 def rotate(matrix):
+    #left and right pointer
     left, right = 0, len(matrix) - 1
 
     while left < right:
+        #top and bottom pointer set to be equal to left and right
+        #because they have the same value and change in the same pace
         top, bottom = left, right
+        #create for loop so the loop gonna get smaller for inner bound
+        #rectangle
+        #also we looping on the counter clockwise so we can save space
+        #we just need one v
         for i in range(right - left):
+            #store top left to the temp topLeft variable
             topLeft = matrix[top][left + i]
+            #change top left to bottom left
             matrix[top][left + i] = matrix[bottom - i][left]
+            #change bottom left to bottom right
             matrix[bottom - i][left] = matrix[bottom][right - i]
+            #change bottom rigth to top right
             matrix[bottom][right - i] = matrix[top + i][right]
+            #change top right to top left which is in the topLeft temp variable
             matrix[top + i][right] = topLeft
+
+        #increase the left and right for the inner ractangle
         left += 1
         right -= 1
-
 
 
 print(matrix)
