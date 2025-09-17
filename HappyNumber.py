9e693e2f9c0d32a1c42a711d6b218baf21384c0f
Leo <alvianleo@gmail.com>
2025-09-17 14:18:56 +0700

    adding HappyNumber.py

---
+++ HappyNumber.py
@@ -0,0 +1,69 @@
+"""
+Write an algorithm to determine if a number n is happy.
+
+A happy number is a number defined by the following process:
+
+Starting with any positive integer, replace the number by the sum of the squares of its digits.
+Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+Those numbers for which this process ends in 1 are happy.
+Return true if n is a happy number, and false if not.
+
+Example 1:
+
+Input: n = 19
+Output: true
+Explanation:
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+
+
+Example 2:
+
+Input: n = 101
+Output: false
+Explanation:
+1^2 + 0^2 + 1^2 = 2
+2^2 = 4
+4^2 = 16
+1^2 + 6^2 = 37
+3^2 + 7^2 = 58
+5^2 + 8^2 = 89
+8^2 + 9^2 = 145
+1^2 + 4^2 + 5^2 = 42
+4^2 + 2^2 = 20
+2^2 + 0^2 = 4 (This number has already been seen)
+"""
+
+n = 19
+output = True
+
+n2 = 101
+output2 = False
+
+
+def happyNum(n):
+    visited = set()
+
+    while n not in visited:
+        visited.add(n)
+        print('visited', visited)
+        n = sumOfSquares(n)
+        print('n',n)
+
+        if n == 1:
+            return True
+    return False
+
+def sumOfSquares(n):
+    out = 0
+
+    while n:
+        mod = n % 10
+        out += mod ** 2
+        n = n // 10
+    return out
+
+
+print(happyNum(n2))
