f1c3def577caf5a3472f6d20dd7243f1313e9bf2
Leo <alvianleo@gmail.com>
2025-09-24 10:21:30 +0700

    Modify NumberOf1Bit.py to be a little bit efficient and also adding comentry

--- NumberOf1Bit.py
+++ NumberOf1Bit.py
@@ -34,12 +34,37 @@ output = 30
 # This work by shifting incrementing res by 1 after we mod n by 2 because we just get
 # the reminder of it and then we shift the bits by one and continue looping
 # But this process we gonna loop through all bits even if it 0
+
+# def hammingWeight(n):
+#     res = 0
+
+#     while n:
+#         res += n % 2
+#         n = n >> 1
+    
+#     return res
+
+
+
+# This function is a bit better because we don't have to loop through all the zero bit
+# We just add 1 to result if the bit is one else we skip it
+# By using n = n & n - 1 we just skip all zero
+# Example
+# n, res = 1001, 0
+# n - 1 => 1001 - 1 => 1000
+# n & n - 1 => 1001 & 1000 => 1000
+# res == 1
+# n - 1 => 1000 - 1 => 0111
+# n & n - 1 => 1000 & 0111 => 0000
+# res == 2
+# end loop because n = 0000 now so we only loop as many as 1 in the bit
+
 def hammingWeight(n):
     res = 0
 
     while n:
-        res += n % 2
-        n = n >> 1
+        n &= n - 1
+        res += 1
     
     return res
 
