a52ad30e87dd00c6048894ddf6a079a5d49efb6e
Leo <alvianleo@gmail.com>
2025-09-24 23:19:55 +0700

    Modify FractionToRecurring.py and add commentry

--- FractionToRecurring.py
+++ FractionToRecurring.py
@@ -32,31 +32,47 @@ def fractionToDecimal(numerator, denominator):
     if numerator == '0':
         return '0'
 
+    # creat list name res and check if numerator or denominator is less than 0
+    # if it is then append - to the res
     res = []
     if numerator < 0 or denominator < 0:
         res.append('-')
-    
+
+    # making numerator and denominator to be positive
     num, den = abs(numerator), abs(denominator)
 
+    # Append string of num / den but only get the whole value
     res.append(str(num // den))
+    # Check if there is a reminder if not then we can just return it
     reminder = num % den
     if reminder == 0:
         return ''.join(res)
     
+    # If there's a reminder then we need to calculate it and append it to res
+    # We also need to check if the reminder is reacurring
+    # If there is, we need to put that in a parenthesis and return that
     res.append('.')
+    # To check if the reminder is reacurring we use hashMap
     rem_map = {}
 
+    # Loop while we have reminder and check if reminder already in hashMap
+    # If so we need to add parenthesis in corresponding index
+    # That's why we need to add len(res) so we can insert parenthesis in the
+    # right position
     while reminder:
         if reminder in rem_map:
             pos = rem_map[reminder]
             res.insert(pos, '(')
             res.append(')')
             break
-
+        
         rem_map[reminder] = len(res)
         reminder *= 10
         res.append(str(reminder // den))
         reminder %= den
+        print('rm', rem_map)
+        print('res', res)
+        print('reminder', reminder)
     
     return ''.join(res)
 
