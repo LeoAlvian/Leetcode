610c168206b36e64e5500d2deb868f594659a074
Leo <alvianleo@gmail.com>
2025-10-04 14:43:12 +0700

    Adding commentry to MajorityElement.py

--- MajorityElement.py
+++ MajorityElement.py
@@ -18,7 +18,9 @@ Constraints:
 Follow-up: Could you solve the problem in linear time and in O(1) space?
 """
 
-
+# Using hash map to map all element in the arr and count how many they are 
+# appearing ex {1:2, 2:1, 3:4} which means 3 is the majority element here 
+# but this approach are using O(n) space and O(n) time complexity 
 def majorityElHash(nums):
     hashMap = {}
     res, maxCount = 0, 0
@@ -31,6 +33,13 @@ def majorityElHash(nums):
     return res
 
 
+# Using Boyer Moore Algorithm to count the majority element with O(1) space complexity
+# this work by counting element that are similar and decrement it if it isn't 
+# and if the count are 0 we gonna replace that element with the new one
+# ex: nums= [1,2,1]
+# count == 0 so change the res to be the first element which is 1 so res = 1 
+# and count == 1, then we see 2 which is different element so we decrement count = 0
+# then the count = 0 and the next element is 1 so we put 1 to res so res = 1
 def majorityEl(nums):
     res, count = 0, 0
 
