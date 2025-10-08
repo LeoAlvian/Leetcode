298a0c221813dc36625c5299ab90a84e588a8ce3
Leo <alvianleo@gmail.com>
2025-10-08 14:23:13 +0700

    Adding SortAnArray.py

---
+++ SortAnArray.py
@@ -0,0 +1,71 @@
+"""
+Given an array of integers nums, sort the array in ascending order and return it.
+
+You must solve the problem without using any built-in functions in O(nlog(n)) time complexity and with the smallest space complexity possible.
+
+ 
+
+Example 1:
+
+Input: nums = [5,2,3,1]
+Output: [1,2,3,5]
+Explanation: After sorting the array, the positions of some numbers are not changed (for example, 2 and 3), while the positions of other numbers are changed (for example, 1 and 5).
+Example 2:
+
+Input: nums = [5,1,1,2,0,0]
+Output: [0,0,1,1,2,5]
+Explanation: Note that the values of nums are not necessarily unique.
+ 
+
+Constraints:
+
+1 <= nums.length <= 5 * 104
+-5 * 104 <= nums[i] <= 5 * 104
+"""
+
+
+class SortArr:
+    def sorta(self, nums):
+        def merge(arr, L, M, R):
+            leftArr, rightArr = arr[L:M+1], arr[M+1:R+1]
+            arrP, Lp, Rp = L, 0, 0
+
+            while Lp < len(leftArr) and Rp < len(rightArr):
+                if leftArr[Lp] <= rightArr[Rp]:
+                    arr[arrP] = leftArr[Lp]
+                    Lp += 1
+                else:
+                    arr[arrP] = rightArr[Rp]
+                    Rp += 1
+                arrP += 1
+            
+            while Lp < len(leftArr):
+                nums[arrP] = leftArr[Lp]
+                Lp += 1
+                arrP += 1
+            
+            while Rp < len(rightArr):
+                nums[arrP] = rightArr[Rp]
+                Rp += 1
+                arrP += 1
+
+
+        def mergeSort(arr, L, R):
+            if L == R:
+                return arr
+            
+            M = (L + R) // 2
+            mergeSort(arr, L, M)
+            mergeSort(arr, M + 1, R)
+            merge(arr, L, M, R)
+            return arr
+
+        return mergeSort(nums, 0, len(nums) - 1)
+
+
+nums = [5,1,1,2,0,0]
+output = [0,0,1,1,2,5]
+
+sa = SortArr()
+print(sa.sorta(nums))
+print(output)
