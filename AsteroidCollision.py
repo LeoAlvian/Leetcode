ab53773b5637235d269166d1f7d36dac655cbde2
Leo <alvianleo@gmail.com>
2025-11-06 13:09:05 +0700

    Modify and adding commentry to AsteroidCollision.py

--- AsteroidCollision.py
+++ AsteroidCollision.py
@@ -47,6 +47,28 @@ Constraints:
 asteroids[i] != 0
 """
 
+# Using Stack data structure to solve this problem
+# asteroids = [3,5,-6,2,-1,4]
+# output = [-6,2,4]
+# stack = []
+# Loop through asteroids and while if there's items in stack and 
+# asteroids[i] is negative and stack[-1] is positive, if abs(asteroids[i]) > stack[-1]
+# then pop from stack, else if abs(asteroids[i] < stack[-1]) destroy the asteroids[i]
+# else pop from stack and destroy asteroids[i]
+# Ex
+# asteroids = [3,5,-6,2,-1,4], a = 3 
+# stack = [3]
+# asteroids = [3,5,-6,2,-1,4] a = 5
+# stack = [3,5]
+# asteroids = [3,5,-6,2,-1,4] a = -6
+# stack = [-6]
+# asteroids = [3,5,-6,2,-1,4] a = 2
+# stack = [-6,3]
+# asteroids = [3,5,-6,2,-1,4] a = -1
+# stack = [-6,2]
+# asteroids = [3,5,-6,2,-1,4] a = 4
+# stack = [-6,2,4]
+# return stack
 
 def AsteroidCollision(asteroids):
     stack = []
