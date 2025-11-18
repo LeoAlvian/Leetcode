31f11f466f0c5ad4fb5dc183c91d9686be83cfbb
Leo <alvianleo@gmail.com>
2025-11-18 13:45:05 +0700

    Adding BinaryTreeInorderTraversal.py

---
+++ BinaryTreeInorderTraversal.py
@@ -0,0 +1,71 @@
+"""
+94. Binary Tree Inorder Traversal
+
+You are given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+Example 1:
+
+Input: root = [1,2,3,4,5,6,7]
+Output: [4,2,5,1,6,3,7]
+
+
+
+Example 2:
+
+Input: root = [1,2,3,null,4,5,null]
+Output: [2,4,1,5,3]
+
+
+
+Example 3:
+Input: root = []
+Output: []
+
+
+Constraints:
+
+0 <= number of nodes in the tree <= 100
+-100 <= Node.val <= 100
+Follow up: Recursive solution is trivial, could you do it iteratively?
+"""
+
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def inorderTraversal(self,root):
+        res = []
+
+        def inorder(node):
+            if not node:
+                return
+            
+            inorder(node.left)
+            res.append(node.val)
+            inorder(node.right)
+        
+        inorder(root)
+        return res
+
+
+
+
+
+list = [1,2,3,4,5,6,7]
+output = [4,2,5,1,6,3,7]
+
+root = Node(1)
+root.left = Node(2)
+root.left.left = Node(4)
+root.left.right = Node(5)
+root.right = Node(3)
+root.right.left = Node(6)
+root.right.right = Node(7)
+
+s = Solution()
+res = s.inorderTraversal(root)
+print(res)
+print(output)
