384235de906814fc3b9901c8a21d8cde66f9eb2a
Leo <alvianleo@gmail.com>
2025-11-22 11:52:49 +0700

    Modify and adding Iterative solution also commentry to InsertBinarySearchTree.py

--- InsertBinarySearchTree.py
+++ InsertBinarySearchTree.py
@@ -38,6 +38,23 @@ It's guaranteed that val does not exist in the original BST.
 """
 
 
+# list = [4,2,7,1,3], val = 5
+#
+#           Tree
+#           [4]           res = [4,2,7,1,3,5]
+#         /     \         stack = []
+#       [2]      [7]
+#      /   \      
+#    [1]  [3]  
+#
+# RESULT
+#           Tree
+#           [4]           res = [1,2,4,5,3,6,7]
+#         /     \         
+#       [2]      [7]
+#      /   \    /   
+#    [1]  [3] [5] 
+
 class TreeNode:
     def __init__(self, val):
         self.val = val
@@ -45,6 +62,11 @@ class TreeNode:
         self.right = None
 
 # RECURSIVE FUNCTION
+# It's Binary Search Tree which means the tree are in order so val to the right of the 
+# root are always bigger and the left always smaller than the root.val 
+# so we gonna check if val is bigger or smaller to determine which direction we move
+# because we using recursive function we need to assign the left or right node to the 
+# recursive function itself that way we gonna assign the value accordingly
 class BST:
     def insertBinarySearchTree(self, root, val):
         if not root:
@@ -56,6 +78,27 @@ class BST:
             root.left = self.insertBinarySearchTree(root.left, val)
         
         return root
+    
+# ITERATIVE SOLUTION
+# Solving iteratively we need to keep track using pointer in this case cur and 
+# traverse it the same way, bigger to the right, smaller to the left until we find a
+# Null node and then insert it there
+    def insertBinarySearchTreeIteratively(self, root, val):
+        if not root:
+            return TreeNode(val)
+        
+        cur = root
+        while True:
+            if val > cur.val:
+                if not cur.right:
+                    cur.right = TreeNode(val)
+                    return root
+                cur = cur.right
+            else:
+                if not cur.left:
+                    cur.left = TreeNode(val)
+                    return root
+                cur = cur.left
     
 
 # TO print the tree into a list we need to traverse it and because the output are 
@@ -89,7 +132,8 @@ root.left.left = TreeNode(1)
 root.left.right = TreeNode(3)
 
 b = BST()
-b.insertBinarySearchTree(root, val)
+# b.insertBinarySearchTree(root, val)
+b.insertBinarySearchTreeIteratively(root, val)
 res = preorderTraversal(root)
 
 print(res)
