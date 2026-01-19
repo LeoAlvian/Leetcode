6b4d3760582ea65c5bd0ffeffea46fb42c793564
Leo <alvianleo@gmail.com>
2026-01-19 14:01:48 +0700

    Adding InsertGCDLinkedList.py, writing GCD algorithm by scratch with time complexity of O(n*log(min(a, b))) and O(1) space complexity

---
+++ InsertGCDLinkedList.py
@@ -0,0 +1,86 @@
+"""
+Docstring for InsertGCDLinkedList
+
+2807. Insert Greatest Common Divisors in Linked List
+
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+
+Given the head of a linked list head, in which each node contains an integer value.
+
+Between every pair of adjacent nodes, insert a new node with a value equal to the greatest common divisor of them.
+
+Return the linked list after insertion.
+
+The greatest common divisor of two numbers is the largest positive integer that evenly divides both numbers.
+
+ 
+
+Example 1:
+Input: head = [18,6,10,3]
+Output: [18,6,6,2,10,1,3]
+Explanation: The 1st diagram denotes the initial linked list and the 2nd diagram denotes the linked list after inserting the new nodes (nodes in blue are the inserted nodes).
+- We insert the greatest common divisor of 18 and 6 = 6 between the 1st and the 2nd nodes.
+- We insert the greatest common divisor of 6 and 10 = 2 between the 2nd and the 3rd nodes.
+- We insert the greatest common divisor of 10 and 3 = 1 between the 3rd and the 4th nodes.
+There are no more adjacent nodes, so we return the linked list.
+
+
+
+Example 2:
+Input: head = [7]
+Output: [7]
+Explanation: The 1st diagram denotes the initial linked list and the 2nd diagram denotes the linked list after inserting the new nodes.
+There are no pairs of adjacent nodes, so we return the initial linked list.
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 5000].
+1 <= Node.val <= 1000
+"""
+
+class Node:
+    def __init__(self, val=0, nxt=None):
+        self.val = val
+        self.next = nxt
+
+def insertGCDLinkedList(head):
+    # Write Greatest Common Divisor by scratch with time complexity of O(logn)
+    def gcd(a, b):
+        while b > 0:
+            a, b = b, a % b
+        return a
+    
+    cur = head
+    while cur.next:
+        v1, v2 = cur.val, cur.next.val
+        cur.next = Node(gcd(v1, v2), cur.next)
+        cur = cur.next.next
+    
+    return head
+    
+
+
+
+list = [18,6,10,3]
+output = [18,6,6,2,10,1,3]
+
+head = Node(18)
+cur = head
+for i in range(1, len(list)):
+    cur.next = Node(list[i])
+    cur = cur.next
+
+igcdl = insertGCDLinkedList(head)
+
+res = []
+while igcdl:
+    res.append(igcdl.val)
+    igcdl = igcdl.next
+
+print(res)
+print(output)
