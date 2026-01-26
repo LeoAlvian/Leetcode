c1af9be979428b41e85ba16d808c48da0fe9e03b
Leo <alvianleo@gmail.com>
2026-01-26 14:12:46 +0700

    Adding GroupAnagrams.py with time complexity of O(m * n)

---
+++ GroupAnagrams.py
@@ -0,0 +1,58 @@
+"""
+Docstring for GroupAnagrams
+
+49. Group Anagrams - Explanation
+
+Description
+Given an array of strings strs, group all anagrams together into sublists. You may return the output in any order.
+
+An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+
+
+Example 1:
+Input: strs = ["act","pots","tops","cat","stop","hat"]
+Output: [["hat"],["act", "cat"],["stop", "pots", "tops"]]
+
+
+
+Example 2:
+Input: strs = ["x"]
+Output: [["x"]]
+
+
+
+Example 3:
+Input: strs = [""]
+Output: [[""]]
+
+
+
+Constraints:
+
+1 <= strs.length <= 1000.
+0 <= strs[i].length <= 100
+strs[i] is made up of lowercase English letters.
+"""
+
+from collections import defaultdict
+
+def groupAnagrams(strs):
+    res = defaultdict(list)
+
+    for s in strs:
+        count = [0] * 26
+        for c in s:
+            count[ord(c) - ord('a')] += 1
+        res[tuple(count)].append(s)
+    
+    return list(res.values())
+
+
+strs = ["act","pots","tops","cat","stop","hat"]
+output = [["hat"],["act", "cat"],["stop", "pots", "tops"]]
+
+ga = groupAnagrams(strs)
+
+print(ga)
+print(output)
