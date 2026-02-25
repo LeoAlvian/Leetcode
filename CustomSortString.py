b3f2610bef4a0f57bd2466cc86c7952f7fa641c1
Leo <alvianleo@gmail.com>
2026-02-25 15:10:45 +0700

    Modify CustomSortString.py and adding another function that preserve the post order, time: O(n), space O(1)

--- CustomSortString.py
+++ CustomSortString.py
@@ -53,7 +53,11 @@ from collections import Counter
 def customSortString(order, s):
     count = Counter(s)
     res = []
-
+    # print(count)
+    # Counter({'q': 11, 'a': 9, 't': 8, 'z': 5, 'd': 5, 'x': 5, 'h': 5, 'o': 4, 'm': 4, 
+    # 'g': 4, 'k': 4, 'l': 4, 'f': 4,'j': 4, 'p': 3, 's': 3, 'c': 3, 'n': 3, 'u': 2, 
+    # 'y': 2, 'b': 2, 'i': 2, 'e': 1, 'v': 1, 'w': 1, 'r': 1})
+    
     for c in order:
         while count[c]:
             res.append(c)
@@ -67,18 +71,41 @@ def customSortString(order, s):
     return ''.join(res)
 
 
+def customSortStringII(order, s):
+    count = [0] * 26
+    for c in s:
+        count[ord(c) - ord('a')] += 1
+    
+    # Map count to it's index with ord(c) - ord('a') => c = b => b - a = 1 so index of 
+    # b is 1
+    # print(count)
+    # [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p,  q, r, s, t, u, p, w, x, y, z]
+    # [9, 2, 3, 5, 1, 4, 4, 5, 2, 4, 4, 4, 4, 3, 4, 3, 11, 1, 3, 8, 2, 1, 1, 5, 2, 5]
+    
+    res = []
+    for c in order:
+        i = ord(c) - ord('a')
+        while count[i]:
+            res.append(c)
+            count[i] -= 1
+    
+    for i in range(26):
+        char = chr(ord('a') + i)
+        while count[i]:
+            res.append(char)
+            count[i] -= 1
+    
+    return ''.join(res)
 
 
-
-order = "bcafg"
-s = "abcd"
-output ="bcad"
 
 order = "hucw"
 s = "utzoampdgkalexslxoqfkdjoczajxtuhqyxvlfatmptqdsochtdzgypsfkgqwbgqbcamdqnqztaqhqanirikahtmalzqjjxtqfnh"
 
 output = "hhhhhuucccwaaaaaaaaabbdddddeffffggggiijjjjkkkkllllmmmmnnnoooopppqqqqqqqqqqqrsssttttttttvxxxxxyyzzzzz"
 
+# With the first method we didn't preserve the order after we finish with order string
 print(customSortString(order, s))
-# print(customSortStringII(order, s))
+# We preserve the order with second function becuase we use ord() function
+print(customSortStringII(order, s))
 print(output)
