6951d7a4e40a05dc55b10fde42710e75fbf0e42c
Leo <alvianleo@gmail.com>
2026-04-18 14:42:23 +0700

    Adding SumRoottoLeafNumbers.py Using DFS with time: O(n) and space O(h)

---
+++ SumRoottoLeafNumbers.py
@@ -0,0 +1,107 @@
+"""
+129. Sum Root to Leaf Numbers
+
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+
+You are given the root of a binary tree containing digits from 0 to 9 only.
+
+Each root-to-leaf path in the tree represents a number.
+
+For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
+Return the total sum of all root-to-leaf numbers. Test cases are generated so that the answer will fit in a 32-bit integer.
+
+A leaf node is a node with no children.
+
+ 
+
+Example 1:
+
+         [1]
+        /   \
+      [2]    [3]
+
+Input: root = [1,2,3]
+Output: 25
+Explanation:
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+Therefore, sum = 12 + 13 = 25.
+
+
+
+Example 2:
+
+         [4]
+        /   \
+      [9]    [0]
+     /   \
+  [5]    [1]
+
+Input: root = [4,9,0,5,1]
+Output: 1026
+Explanation:
+The root-to-leaf path 4->9->5 represents the number 495.
+The root-to-leaf path 4->9->1 represents the number 491.
+The root-to-leaf path 4->0 represents the number 40.
+Therefore, sum = 495 + 491 + 40 = 1026.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 1000].
+0 <= Node.val <= 9
+The depth of the tree will not exceed 10.
+"""
+
+
+
+# Using DFS
+class TreeNode:
+    def __init__(self, val = 0, left = None, right = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def sumNumbers(self, root):
+
+        def dfs(cur, num):
+            if not cur:
+                return 0
+            
+            num = num * 10 + cur.val
+            if not cur.left and not cur.right:
+                return num
+            return dfs(cur.left, num) + dfs(cur.right, num)
+
+        return dfs(root, 0)
+
+
+
+
+
+#          [4]
+#         /   \
+#       [9]    [0]
+#      /   \
+#   [5]    [1]
+  
+arr = [4,9,0,5,1]
+output = 1026
+
+root = TreeNode(4)
+root.left = TreeNode(9)
+root.right = TreeNode(0)
+root.left.left = TreeNode(5)
+root.left.right = TreeNode(1)
+
+s = Solution()
+res = s.sumNumbers(root)
+
+print(res)
+print(output)
