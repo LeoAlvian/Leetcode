b17f03534e44dda335def9023b5e6ca57a249d35
Leo <alvianleo@gmail.com>
2026-04-23 15:08:23 +0700

    Adding ConvertBSTtoGreaterTree.py using DFS and print with BFS with time: O(n) and space: O(h)

---
+++ ConvertBSTtoGreaterTree.py
@@ -0,0 +1,123 @@
+"""
+538. Convert BST to Greater Tree
+
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+
+Given the root of a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to the original key plus the sum of all keys greater than the original key in BST.
+
+As a reminder, a binary search tree is a tree that satisfies these constraints:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+ 
+
+Example 1:
+
+               [4] 30
+            /         \
+          [1] 36       [6] 21
+        /    \       /    \
+      [0] 36 [2] 35 [5] 26 [7] 15
+               \             \
+               [3] 33        [8]
+
+Input: root = [4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]
+Output: [30,36,21,36,35,26,15,null,null,null,33,null,null,null,8]
+
+
+
+
+Example 2:
+
+Input: root = [0,null,1]
+Output: [1,null,1]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 104].
+-104 <= Node.val <= 104
+All the values in the tree are unique.
+root is guaranteed to be a valid binary search tree.
+ 
+
+Note: This question is the same as 1038: https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/
+"""
+
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def convertBST(self, root):
+        curSum = 0
+
+        def dfs(node):
+            if not node:
+                return 
+            
+            nonlocal curSum
+            dfs(node.right)
+            tmp = node.val
+            node.val += curSum
+            curSum += tmp
+            dfs(node.left)
+
+        dfs(root)
+        return root
+    
+
+    def printLevel(self, root):
+        q = deque([root] if root else None)
+        res = []
+
+        while q:
+            for i in range(len(q)):
+                node = q.popleft()
+                res.append(node.val)
+
+                if node.left: q.append(node.left)
+                if node.right: q.append(node.right)
+        
+        return res
+
+
+
+#               [4] 30
+#             /         \
+#           [1] 36       [6] 21
+#         /    \       /    \
+#       [0] 36 [2] 35 [5] 26 [7] 15
+#                \             \
+#                [3] 33        [8]
+
+
+arr = [4,1,6,0,2,5,7,None,None,None,3,None,None,None,8]
+output = [30,36,21,36,35,26,15,None,None,None,33,None,None,None,8]
+
+root = TreeNode(4)
+root.left = TreeNode(1)
+root.right = TreeNode(6)
+root.left.left = TreeNode(0)
+root.left.right = TreeNode(2)
+root.left.right.right = TreeNode(3)
+root.right.left = TreeNode(5)
+root.right.right = TreeNode(7)
+root.right.right.right = TreeNode(8)
+
+s = Solution()
+newRoot = s.convertBST(root)
+res = s.printLevel(newRoot)
+
+print(res)
+print(output)
