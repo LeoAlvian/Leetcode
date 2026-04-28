a939d880541861f491021a1f8635e48edf09b8af
Leo <alvianleo@gmail.com>
2026-04-28 14:52:08 +0700

    Adding CountingWordsWithaGivenPrefix.py with time: O(m * n) and space: O(1)

---
+++ CountingWordsWithaGivenPrefix.py
@@ -0,0 +1,63 @@
+"""
+2185. Counting Words With a Given Prefix
+
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+
+You are given an array of strings words and a string pref.
+
+Return the number of strings in words that contain pref as a prefix.
+
+A prefix of a string s is any leading contiguous substring of s.
+
+ 
+
+Example 1:
+
+Input: words = ["pay","attention","practice","attend"], pref = "at"
+Output: 2
+Explanation: The 2 strings that contain "at" as a prefix are: "attention" and "attend".
+
+
+
+Example 2:
+
+Input: words = ["leetcode","win","loops","success"], pref = "code"
+Output: 0
+Explanation: There are no strings that contain "code" as a prefix.
+ 
+
+Constraints:
+
+1 <= words.length <= 100
+1 <= words[i].length, pref.length <= 100
+words[i] and pref consist of lowercase English letters.
+"""
+
+
+# Time complexity is O(m * n) Where m is the number of words and n is the length of the
+# string pref. because we use brute force and space is O(1)
+# This is fast for easy problem because the input is small
+def prefixCount(words, pref):
+    N = len(pref)
+    res = 0
+
+    for word in words:
+        if word[:N] == pref:
+            res += 1
+
+    return res
+
+
+words = ["pay","attention","practice","attend"]
+pref = "at"
+output = 2
+
+res = prefixCount(words, pref)
+
+print(res)
+print(output)
