45f12d4dda70e2796ed8631a75342f9240f721a4
Leo <alvianleo@gmail.com>
2026-04-29 17:37:38 +0700

    Adding FloodFill.py Using BFS to traverse the grid with time: O(m * n) and space: O(m * n)

---
+++ FloodFill.py
@@ -0,0 +1,82 @@
+"""
+733. Flood Fill
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+You are given an image represented by an m x n grid of integers image, where image[i][j] represents the pixel value of the image. You are also given three integers sr, sc, and color. Your task is to perform a flood fill on the image starting from the pixel image[sr][sc].
+
+To perform a flood fill:
+
+Begin with the starting pixel and change its color to color.
+Perform the same process for each pixel that is directly adjacent (pixels that share a side with the original pixel, either horizontally or vertically) and shares the same color as the starting pixel.
+Keep repeating this process by checking neighboring pixels of the updated pixels and modifying their color if it matches the original color of the starting pixel.
+The process stops when there are no more adjacent pixels of the original color to update.
+Return the modified image after performing the flood fill.
+
+ 
+
+Example 1:
+
+Input: image = [[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, color = 2
+Output: [[2,2,2],[2,2,0],[2,0,1]]
+
+Explanation:
+
+image =  [1,1,1]          [2,2,2]
+         [1,1,0]    ->    [2,2,0]
+         [1,0,1]          [2,0,1]
+
+From the center of the image with position (sr, sc) = (1, 1) (i.e., the red pixel), all pixels connected by a path of the same color as the starting pixel (i.e., the blue pixels) are colored with the new color.
+
+Note the bottom corner is not colored 2, because it is not horizontally or vertically connected to the starting pixel.
+
+
+
+Example 2:
+
+Input: image = [[0,0,0],[0,0,0]], sr = 0, sc = 0, color = 0
+Output: [[0,0,0],[0,0,0]]
+
+Explanation:
+
+The starting pixel is already colored with 0, which is the same as the target color. Therefore, no changes are made to the image.
+"""
+
+
+from collections import deque
+
+def floodFill(image, sr, sc, color):
+    ori = image[sr][sc]
+    if ori == color:
+        return image
+    
+    row, col = len(image), len(image[0])
+    q = deque([(sr, sc)])
+    image[sr][sc] = color
+    dirs = [(1,0), (0,1), (-1,0), (0,-1)]
+
+    while q:
+        r, c = q.popleft()
+        for dr, dc in dirs:
+            newR, newC = r + dr, c + dc
+            if 0 <= newR < row and 0 <= newC < col and image[newR][newC] == ori:
+                image[newR][newC] = color
+                q.append((newR, newC))
+
+    return image
+
+
+
+image = [[1,1,1],[1,1,0],[1,0,1]]
+sr = 1
+sc = 1
+color = 2
+output = [[2,2,2],[2,2,0],[2,0,1]]
+
+res = floodFill(image, sr, sc, color)
+
+print(res)
+print(output)
