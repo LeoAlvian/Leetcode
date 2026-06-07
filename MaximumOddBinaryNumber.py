dc29d0396b88efb33d426efcde51c9f4cbb107d9
Leo <alvianleo@gmail.com>
2026-06-08 01:48:51 +0700

    Modify MaximumOddBinaryNumber.py Using Greedy Approach, slightly faster on leetcode, with time: O(n) and space: O(n)

--- MaximumOddBinaryNumber.py
+++ MaximumOddBinaryNumber.py
@@ -58,10 +58,27 @@ def maximumOddBinaryNumber(s):
     return ''.join(ls)
 
 
+# Using Greedy Approach, we count every '1' and just subtract one to be placed in the right 
+# most side of the string and the rest gonna be the right most side and the fill the middle 
+# with '0'
+# Also this solution is slightly faster on leetcode
+
+def maximumOddBinaryNumberII(s):
+    count = 0  # Count every one on the string s
+
+    for c in s:
+        if c == '1':
+            count += 1
+    
+    return '1' * (count - 1) + '0' * (len(s) - count) +'1'
+
+
 s = "0101"
 output = "1001"
 
 res = maximumOddBinaryNumber(s)
+res2 = maximumOddBinaryNumberII(s)
 
 print(res)
+print(res2)
 print(output)
