f631512ee580d3732e547f344f8302194c2172d5
Leo <alvianleo@gmail.com>
2026-06-18 14:01:40 +0700

    CountOddNumberisanIntervalRange.py Using Interval count with time: O(1) and space: O(1)

---
+++ CountOddNumbersinanIntervalRange.py
@@ -0,0 +1,60 @@
+"""
+1523. Count Odd Numbers in an Interval Range
+
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+
+Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive).
+
+ 
+
+Example 1:
+
+Input: low = 3, high = 7
+Output: 3
+Explanation: The odd numbers between 3 and 7 are [3,5,7].
+
+
+
+Example 2:
+
+Input: low = 8, high = 10
+Output: 1
+Explanation: The odd numbers between 8 and 10 are [9].
+
+
+Example 3:
+
+low = 3, high = 21
+Output: 10
+Explanation: The odd numbers between 3 and 21 are [3,5,7,9,11,13,15,17,19,21].
+ 
+
+Constraints:
+
+0 <= low <= high <= 10^9
+"""
+
+
+
+def countOdds(low, high):
+    inter = high - low + 1
+    count = inter // 2
+
+    if inter % 2 and low % 2:
+        count += 1
+
+    return count
+
+low = 3
+high = 21
+output = 10
+
+res = countOdds(low, high)
+
+print(res)
+print(output)
