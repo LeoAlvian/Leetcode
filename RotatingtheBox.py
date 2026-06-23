a744e759f3dc8608bb51bc542004990f26a5cbfd
Leo <alvianleo@gmail.com>
2026-06-23 20:10:38 +0700

    Modify RotatingtheBox.py with Grid manipulation in place with time: O(m * n) and space: O(m * n) with O(1) exptra space

--- RotatingtheBox.py
+++ RotatingtheBox.py
@@ -64,6 +64,31 @@ boxGrid[i][j] is either '#', '*', or '.'.
 """
 
 
+# Swap in place with time O(m * n) and space O(1)
+def rotateTheBox(boxGrid):
+    ROWS, COLS = len(boxGrid), len(boxGrid[0])
+
+    # swapping the stones in place
+    for r in range(ROWS):
+        i = COLS - 1
+        for c in reversed(range(COLS)):
+            if boxGrid[r][c] == "#":
+                boxGrid[r][c], boxGrid[r][i] = boxGrid[r][i], boxGrid[r][c]
+                i -= 1
+            elif boxGrid[r][c] == "*":
+                i = c - 1
+
+    # rotating the matrix
+    res = []
+    for c in range(COLS):
+        col = []  # this is a row after rotation
+        for r in reversed(range(ROWS)):
+            col.append(boxGrid[r][c])
+        res.append(col)
+
+    return res
+
+
 
 def rotateTheBox(boxGrid):
     ROWS, COLS = len(boxGrid), len(boxGrid[0])
