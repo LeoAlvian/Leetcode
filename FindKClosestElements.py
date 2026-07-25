e412af2f74143be9bc12d46cb3ea5b3301f8dbd9
Leo <alvianleo@gmail.com>
2026-07-25 12:01:03 +0700

    FindKClosestElement.py Using Sliding Windows and Binary Search with time: O(log(n + k) + k) and space: O(k)

---
+++ FindKClosestElements.py
@@ -0,0 +1,65 @@
+"""
+658. Find K Closest Elements
+
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+
+Given a sorted integer array arr, two integers k and x, return the k closest integers to x in the array. The result should also be sorted in ascending order.
+
+An integer a is closer to x than an integer b if:
+
+|a - x| < |b - x|, or
+|a - x| == |b - x| and a < b
+ 
+
+
+Example 1:
+
+Input: arr = [1,2,3,4,5], k = 4, x = 3
+Output: [1,2,3,4]
+
+
+
+Example 2:
+
+Input: arr = [1,1,2,3,4,5], k = 4, x = -1
+Output: [1,1,2,3]
+
+ 
+
+Constraints:
+
+1 <= k <= arr.length
+1 <= arr.length <= 104
+arr is sorted in ascending order.
+-104 <= arr[i], x <= 104
+"""
+
+
+# Using Sliding Windows and Binary Search with time: O(log(n + k) + k) and space: O(k)
+
+def findClosestElements(arr, k, x):
+    l, r = 0, len(arr) - k
+
+    while l < r:
+        mid = (l + r) >> 1
+        if x - arr[mid] > arr[mid + k] - x:
+            l = mid + 1
+        else:
+            r = mid
+    
+    return arr[l:l + k]
+
+
+arr = [1,2,3,4,5]
+k = 4
+x = 3
+output = [1,2,3,4]
+
+res = findClosestElements(arr, k, x)
+
+print(res)
+print(output)
